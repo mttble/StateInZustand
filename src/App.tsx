86f97e7b185689c9603f9bc7d1bbdc00e68e51cd
Matthew Beale <129032908+mttble@users.noreply.github.com>
2024-03-20 09:35:39 +1030

    Counter implementing global state achieved with zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import { useState } from 'react'
 import './App.css'
+import { useCounterStore } from './store'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+const count = useCounterStore(state => state.count)
 
+return <OtherComponent count={count} />
+}
+
+const OtherComponent = ({ count }: { count: number }) => { 
+  const increment = useCounterStore(state => state.increment)
+  const decrement = useCounterStore(state => state.decrement)
   return (
-    <>
-      <h1>Zustand Counter</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <div>
+      {count}
+      <div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
       </div>
-    </>
+    </div> 
   )
 }
 
